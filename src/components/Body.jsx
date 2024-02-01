775bcfb239aea930d8a0697a9af2c665bdbbce54
madhurja <masshuramadhurja99@gmail.com>
2024-02-01 22:51:06 +0600

    search button added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,40 @@
+import { useState } from "react";
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
 
+function filterData(searchText, restaurant){
+  restaurant.filter()
+}
+
 const Body = () => {
-  const searchText= "kfc";
+ 
+  const [restaurant,setResturant]=useState(restaurantList)
+  const [searchText, setSearchText] = useState("");
+
   return (
     <>
-   <div className="search-container">
-    <input type="text" className="search-input"  placeholder="Search" value={searchText} />
-    console.log({searchText} )
-    <button className="search-button" >Search</button>
-   </div>
-
-    
-
-
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+          setSearchText(e.searchText.value)
+          }}
+        />
+        <h1>{searchText}</h1>
+        <h2>{clicked}</h2>
+        {/*  */}
+        <button
+          className="search-button"
+          onClick={() => {
+            const data= filterData(searchText,restaurant)
+          }}
+        >
+          Search
+        </button>
+      </div>
 
       <div className="restaurant-list">
         {restaurantList.map((restaurant) => {
