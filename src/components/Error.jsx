b4946b5042265a88d1503fc7de7d18b9c2542802
madhurja <masshuramadhurja99@gmail.com>
2024-02-06 12:18:28 +0600

    Error.js added

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { useRouteError } from "react-router-dom"}
 
 const Error= () => {
+  const err=useRouteError();
+  console.log(err)
   return (
     <div>
       <h1>Opps!!!</h1>
